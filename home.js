9d01815a3d6fdb8ea6c287ac1636102f298a0400
Laramie Downs <laramiedowns1994@gmail.com>
2021-11-19 10:43:56 -0600

    Completed home.js

--- home.js
+++ home.js
@@ -22,9 +22,9 @@
 */
 
 //CODE HERE
+const greetUser = username => console.log(`Welcome back, ${username}`)
 
-
-
+//greetUser('Andrew')
 
 
 //////////////////PROBLEM 2////////////////////
@@ -50,8 +50,15 @@
 const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
 //CODE HERE
-
-
+const canWeDeliver = zipCode => {
+    if(deliveryAreaZipCodes.includes(zipCode)){
+        console.log(`You're in our delivery zone!`)
+    }else{
+        console.log(`Sorry, we can't deliver to that address`)
+    }
+}
+//canWeDeliver(84606)
+//canWeDeliver(85205)
 
 /* 
     Problem 2 Continued
@@ -71,8 +78,17 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 */
 
 // CODE HERE
-
-
+const canWeDeliverTwo = zipCode => {
+    for(let i = 0; i < deliveryAreaZipCodes.length; i++){
+        if(deliveryAreaZipCodes[i] === zipCode){
+            console.log(`You're in our delivery zone!`)
+            return
+        }
+    }
+    console.log(`Sorry, we can't deliver to that address`)
+}
+// canWeDeliverTwo(84606)
+// canWeDeliverTwo(85205)
 //////////////////PROBLEM 3////////////////////
 /* 
     Below is an array of objects that have some
@@ -107,7 +123,7 @@ const deals = [
 */
 
 //CODE HERE
-
+//console.log(deals[0].title.replace('15', '10'))
 
 
 /*
@@ -124,3 +140,4 @@ const deals = [
 */
 
 //CODE HERE
+console.log(deals[1].desc.replace('March', 'April').trim())
